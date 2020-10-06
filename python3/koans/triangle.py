3cede285f0aebc0278a15ee2e58b1cfdfcc8807f
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-10-06 15:25:00 -0400

    50% completed

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,8 +17,20 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+
+    if a <= 0 or b <= 0 or c <= 0:
+        raise TriangleError(AttributeError('not right'))
+
+    if a > (b +c) or b > (a + c) or c > (a + b):
+        raise TriangleError(AttributeError('not right'))
+
+    if a == b & b == c:
+        return 'equilateral'
+    elif a == b or b == c or c == a:
+        return 'isosceles'
+    else:
+        return 'scalene'
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
